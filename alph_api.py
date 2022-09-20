e93d1e68bf754bd554d7c2b7ab60fecaf816c9de
Liz Cheng <70891408+azile-g@users.noreply.github.com>
2022-09-21 01:15:37 +0800

    Add files via upload

---
+++ alph_api.py
@@ -0,0 +1,7 @@
+import requests
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+print(data)
